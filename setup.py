7bd5997edd9628b2663b51348247054bc7840f5c
burke86 <colin.burke.j@gmail.com>
2025-05-03 00:57:53 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
-#!/usr/bin/env python
-import os
-import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
-from src.pyqsofit.version import __version__
-
-
-# Load the __version__ variable without importing the package already
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(name='EzTaoX',
-      version=__version__,
-      description="",
-      long_description=long_description,
-      author='',
-      author_email='',
-      url='',
-      license='GNU General Public License v3.0',
-      package_dir={'eztaox': 'src/eztaox'},
-      packages=['eztaox'],
-      install_requires=install_requires,
-      include_package_data=True,
-      classifiers=[
-          "License :: OSI Approved :: GNU License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Intended Audience :: Science/Research",
-          "Topic :: Scientific/Engineering :: Astronomy",
-          ],
-      )
+setup(
+    name="eztaox",
+    version="0.1.0",
+    description="A package for multiband quasar light curve modeling and analysis.",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/eztaox",  # Replace with your repository URL
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "jax",
+        "jaxlib",
+        "numpyro",
+        "tinygp",
+        "optax",
+        "equinox",
+        "astropy",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "tqdm",
+        "h5py",
+        "blackjax",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "eztaox=eztaox.main:main",  # Replace `eztaox.main:main` with the actual entry point
+        ],
+    },
+)
