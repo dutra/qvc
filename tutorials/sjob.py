69f62785e98346588ef23f081f5ed8be3ad1b77e
Isaque Dutra <isaque.dutra@yale.edu>
2025-05-17 10:07:55 -0400

    add prefix to sjob

--- tutorials/sjob.py
+++ tutorials/sjob.py
@@ -3,15 +3,15 @@ import glob
 import pandas as pd
 
 # Parameters
-chunk_size = 3000      # Number of objects per job
+chunk_size = 800      # Number of objects per job
 prefix = "may16"
-#suffix = "etasep_colinpriors_all"
-suffix = "singlepl2_all"
+suffix = "latent"
+#suffix = "singlepl2_all"
 lc_file = "data/may8_lc_all.h5"
-#filter_file = "data/df_quasars_filtered_apr29.csv"
-filter_file = "data/all_object_ids.csv"
+filter_file = "data/df_quasars_filtered_apr29.csv"
+#filter_file = "data/all_object_ids.csv"
 script_path = "submit_jobs"
-output_glob = f"data/{prefix}_objs_tauwavelength_taublr_{suffix}_*.h5"
+output_glob = f"data/{prefix}_objs_{suffix}_*.h5"
 log_glob = f"{script_path}/{prefix}_gpu_job_{suffix}_*.txt"
 
 def delete_existing_outputs():
@@ -37,11 +37,11 @@ print(f"Found {total_objects} objects in {filter_file}")
 for i, skip in enumerate(range(0, total_objects, chunk_size)):
     sbatch_filename = os.path.join(script_path, f"{prefix}_job_{suffix}_{i}.sh")
     output_filename = f"{script_path}/{prefix}_gpu_job_{suffix}_{i}.txt"
-    result_file = f"data/{prefix}_objs_tauwavelength_taublr_{suffix}_{i}.h5"
+    result_file = f"data/{prefix}_objs_{suffix}_{i}.h5"
 
     with open(sbatch_filename, "w") as f:
         f.write(f"""#!/bin/bash
-#SBATCH --job-name={prefix}_multiband_fit_taublr_{suffix}_{i}
+#SBATCH --job-name={prefix}_multiband_fit_{suffix}_{i}
 #SBATCH --output={output_filename}
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -59,7 +59,7 @@ module load miniconda
 conda activate jaxgpu
 
 python multiband_fit.py --skip {skip} --N {chunk_size} --lc_file {lc_file} \\
-    --filter_file {filter_file} --file {result_file} --plot --nwarm 200 --nsamp 100
+    --filter_file {filter_file} --file {result_file} --plot --nwarm 150 --nsamp 50 --latent
 """)
 # 
     os.system(f"sbatch {sbatch_filename}")
