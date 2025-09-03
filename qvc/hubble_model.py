3696c0b1465fcd180485319541f46653a5702687
burke86 <colin.burke.j@gmail.com>
2025-09-03 15:43:03 -0400

    cov term in hd

--- qvc/hubble_model.py
+++ qvc/hubble_model.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 # --- ONE SOURCE OF TRUTH (orders) ---
 agn_model_req_params = ("M0_agn", "alpha_agn", "beta_agn")
 ag_model_req_obs     = ("log_sigma_UV", "log_tau_UV_RF", "alpha_nu")
-agn_model_req_errs   = ("log_sigma_UV_err", "log_tau_UV_RF_err", "alpha_nu_err")
+agn_model_req_errs   = ("log_sigma_UV_err", "log_tau_UV_RF_err", "alpha_nu_err", "cov_log_sigma_UV_log_tau_UV_RF")
 
 # Build index maps once (module import time)
 agn_model_pidx = {k:i for i,k in enumerate(agn_model_req_params)}
@@ -56,10 +56,12 @@ def M_model_agn_err(params_arr, obs_arr, err_arr, pivots_array):
 
     log_sigma_UV_err  = err_arr[agn_model_eidx["log_sigma_UV_err"]]
     log_tau_UV_RF_err = err_arr[agn_model_eidx["log_tau_UV_RF_err"]]
+    cov_log_sigma_tau = err_arr[agn_model_eidx["cov_log_sigma_UV_log_tau_UV_RF"]]
 
     return np.sqrt(
         (alpha_agn * log_sigma_UV_err)**2
         + (beta_agn  * log_tau_UV_RF_err)**2
+        + 2 * alpha_agn * beta_agn * cov_log_sigma_tau
     )
 
 # def broken_power_law_err(x, x_err, x_break, d1, d2, ds):
