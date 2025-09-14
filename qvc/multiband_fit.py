93fe628709a5f84c3070f4568d04b28e36aec9db
burke86 <colin.burke.j@gmail.com>
2025-09-14 15:03:58 -0400

    adjust priors

--- qvc/multiband_fit.py
+++ qvc/multiband_fit.py
@@ -355,17 +355,17 @@ def build_model(batch_data, zs, lam_rfs, f_host_value, log_jitter_mean, log_tau_
                 lag_center   = jnp.log(0.2) + log_tau_drw0[..., None]
                 log_lag_blr  = numpyro.sample(
                     "log_lag_blr",
-                    dist.TruncatedNormal(lag_center, 1.0, low=jnp.log(0.2), high=jnp.log(1_000.0))
+                    dist.Uniform(jnp.log(0.2), jnp.log(5000.0))
                 )
-                width_center = jnp.log(0.2) + log_tau_drw0[..., None]
-                width_blr  = numpyro.sample("width_blr",  dist.LogNormal(width_center, 1.0))
-                width_cont = numpyro.sample("width_cont", dist.LogNormal(width_center - jnp.log(2.0), 1.0))
+                #width_center = jnp.log(0.2) + log_tau_drw0[..., None]
+                #width_blr  = numpyro.sample("width_blr",  dist.LogNormal(width_center, 1.0))
+                #width_cont = numpyro.sample("width_cont", dist.LogNormal(width_center - jnp.log(2.0), 1.0))
 
                 #width_blr = numpyro.sample("width_blr", dist.TruncatedNormal(0.2 * jnp.exp(log_tau_drw0_c[..., None]), 20.0, low=10.0))
                 #width_cont = numpyro.sample("width_cont", dist.TruncatedNormal(0.2 * jnp.exp(log_tau_drw0_c[..., None]), 20.0, low=10.0))
 
-                #width_blr = numpyro.deterministic("width_blr", 0.2 * jnp.exp(log_tau_drw0[..., None]))
-                #width_cont = numpyro.deterministic("width_cont", 0.0 * jnp.exp(log_tau_drw0_c[..., None]))
+                width_blr = numpyro.deterministic("width_blr", 0.2 * jnp.exp(log_tau_drw0_c[..., None]))
+                width_cont = numpyro.deterministic("width_cont", 0.0 * jnp.exp(log_tau_drw0_c[..., None]))
 
                 # Jitter
                 log_jitter = numpyro.sample("log_jitter", dist.Normal(log_jitter_mean, 1.0))
