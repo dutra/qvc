6402f14da94ffe5d62b1727ce1303a1935757172
burke86 <colin.burke.j@gmail.com>
2025-09-15 17:30:10 -0400

    merge

--- qvc/multiband_fit.py
+++ qvc/multiband_fit.py
@@ -130,18 +130,8 @@ def build_model(batch_data, zs, lam_rfs, f_host_value, log_jitter_mean, log_tau_
         eta_tau1_mean = numpyro.sample("eta_tau1_mean", dist.Uniform(-1.0, 5.0))
         eta_tau2_mean = numpyro.sample("eta_tau2_mean", dist.Uniform(-1.0, 5.0))
 
-<<<<<<< HEAD
-        # Symmetric, order-agnostic priors for global tau-slopes
-        #mu_eta_tau = numpyro.sample("mu_eta_tau", dist.Normal(0.5, 2.0))   # broad center near what you expect
-        #delta_eta_tau = numpyro.sample("delta_eta_tau", dist.Normal(0.0, 2.0))  # symmetric around 0
-
-        #eta_tau1_mean = numpyro.deterministic("eta_tau1_mean", mu_eta_tau + 0.5 * delta_eta_tau)
-        #eta_tau2_mean = numpyro.deterministic("eta_tau2_mean", mu_eta_tau - 0.5 * delta_eta_tau)
-
-=======
         eta_tau1_mean = numpyro.sample("eta_tau1_mean", dist.Uniform(-1.0, 5.0))
         eta_tau2_mean = numpyro.sample("eta_tau2_mean", dist.Uniform(-1.0, 5.0))
->>>>>>> 6e94c52750396bef1ecfca07b81f5f69c90caec4
 
         if free_eta_break:
             print("[INFO] Free eta_break and lam_s.")
