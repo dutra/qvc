be2e6a2981356f1f6375aa14e138f3d005971ac6
Isaque Dutra <isaque.dutra@protonmail.com>
2025-10-06 02:23:20 -0400

    PL_break_wave free

--- qvc/fit_spectra.py
+++ qvc/fit_spectra.py
@@ -334,7 +334,7 @@ def create_qsopar_fits(path_ex='data/', parfilename='qsopar.fits', overwrite=Tru
         ('PL_norm',      1.0,   0.0,   1e10,  1),  # Power-law normalization (f_λ ∝ (λ/3000)^-α)
         ('PL_slope_blue',    -1.5,  -5.0,  0.0,   1), # Blue slope of the power-law (PL) continuum
         ('PL_slope_red',     -0.5,  -5.0,  0.0,   1), # Red slope of the power-law (PL) continuum
-        ('PL_break_wave',    4000,  3000,  5000,  0), # Break wavelength of the power-law (PL) continuum
+        ('PL_break_wave',    4000,  3000,  5000, 1), # Break wavelength of the power-law (PL) continuum
         ('Balmer_norm',  0.0,   0.0,   1e10,  1),  # Balmer continuum normalization (< 3646 Å)
         ('Balmer_Te',  15000, 10000, 50000,  1),   # Balmer continuum Te
         ('Balmer_Tau',   0.5,   0.1,   2.0,   1),  # Balmer continuum τ
