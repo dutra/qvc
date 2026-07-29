7de8e48b8be0f9cbec0367ba8b7e97907496eb80
Isaque Dutra <isaque.dutra@protonmail.com>
2026-07-28 22:13:05 -0400

    Hide residuals in joint spectrum plots

--- tests/test_fit_spectra_jaxsedfit_joint.py
+++ tests/test_fit_spectra_jaxsedfit_joint.py
@@ -91,9 +91,11 @@ def test_save_spectrum_figure_uses_separate_spectrum_filename(tmp_path):
     class FakeFitter:
         def __init__(self):
             self.show_plot = None
+            self.plot_residual = None
 
-        def plot_jaxqsofit_spectrum(self, *, show_plot):
+        def plot_jaxqsofit_spectrum(self, *, show_plot, plot_residual):
             self.show_plot = show_plot
+            self.plot_residual = plot_residual
             return plt.figure()
 
     fitter = FakeFitter()
@@ -104,5 +106,6 @@ def test_save_spectrum_figure_uses_separate_spectrum_filename(tmp_path):
     )
 
     assert fitter.show_plot is False
+    assert fitter.plot_residual is False
     assert path == tmp_path / "z0.300_205105.02-003302.7_spectrum.png"
     assert path.is_file()
